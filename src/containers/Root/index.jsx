7657c0420e4547b41c83d71c4bdc7ceeacea3401
mateicaba <mateicaba@gmail.com>
2023-01-02 23:40:30 +0300

    new commit

--- src/containers/Root/index.jsx
+++ src/containers/Root/index.jsx
@@ -1,15 +1,58 @@
 import React from "react";
-import { Layout } from "antd";
+import { Layout, Menu } from "antd";
 
-function Root() {
-  return (
-    <Layout>
-      <Layout.Sider>Sider</Layout.Sider>
-      <Layout.Header>Header</Layout.Header>
-      <Layout.Content>Content</Layout.Content>
-      <Layout.Footer>Footer</Layout.Footer>
-    </Layout>
-  );
+const { Header, Sider } = Layout;
+
+class Root extends React.Component {
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
+  render() {
+    return (
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+            <Menu.Item key="1">
+              <a href="/">Dashboard</a>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <a href="/users">Users</a>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <a href="/posts">Posts</a>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: "#fff", padding: 0 }}>
+            <div style={{ float: "left" }}>
+              <a href="/">
+                <h1 style={{ color: "#000", margin: "16px 24px 16px 0" }}>
+                  My App
+                </h1>
+              </a>
+            </div>
+            <div style={{ float: "right" }}>
+              <a href="/logout">
+                <h1 style={{ color: "#000", margin: "16px 24px 16px 0" }}>
+                  Logout
+                </h1>
+              </a>
+            </div>
+          </Header>
+          {this.props.children}
+        </Layout>
+      </Layout>
+    );
+  }
 }
 
 export default Root;
